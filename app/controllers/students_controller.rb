9c5d42a88bb4ea921d17ad5bafe86e0ba56bbf69
sajidnwz <sajidnwz72@gmail.com>
2019-05-22 22:42:58 +0500

    students crud for apis

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,45 @@
 class StudentsController < ApplicationController
+  
+  before_action :load_student, only: %i(show update destroy)
+
   def index
+  	@students = Student.all
+  	render json: {success: true, message: "Loading students", data: @students.as_json}
   end
 
-  def new
+  def create
+  	@student = Student.new(student_params)
+  	if @student.save
+  		render json: {success: true, message: "Student Details", data: @student.as_json}
+  	else
+  		render json: {success: false, message: "Erros", errors: @student.errors.full_messages}
+  	end
   end
 
-  def edit
+  def update
+ 		if @student.update_attributes(student_params)
+  		render json: {success: true, message: "Student Updated successfully", data: @student.as_json}
+  	else
+  		render json: {success: false, message: "Erros", errors: @student.errors.full_messages}
+  	end	 	
+  end
+
+  def destroy
+  	@student.destroy
+  	render json: {success: true, message: "Student Deleted successfully"}
   end
 
   def show
+  	render json: {success: true, message: "Student Details", data: @student.as_json}
+  end
+
+  private
+
+  def load_student
+  	@student = Student.find(params[:id])
+  end
+
+  def student_params
+  	params.require(:student).permit(:name, :roll_no)
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,7 @@
 class Student < ApplicationRecord
 	has_many :results
 	belongs_to :klass, optional: true
+
+	validates :name, :roll_no, presence: true
+	validates :roll_no, uniqueness: true
 end
